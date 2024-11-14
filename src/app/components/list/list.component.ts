65f12e26b4aee910d1e8e0d92332ede1815fc596
dusssk12 <144816333+dusssk12@users.noreply.github.com>
2024-11-14 19:11:13 +0100

    Add list component

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,25 @@
+// dropdown.component.ts
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cyberfab-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+  
+  isOpen = false;
+  selectedOption: string | null = null;
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isOpen = false;
+  }
+}
